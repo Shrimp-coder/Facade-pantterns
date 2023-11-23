2e1a919977dd7a02645b1a40449e6ec2a721e2b5
Shrimp-coder <tranan2571@gmail.com>
2023-11-23 22:23:25 +0700

    Code-facade

---
+++ Facade/Facade/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadeDesignPattern
+{
+    public class Invoice
+    {
+        public void Sendinvoice()
+        {
+            Console.WriteLine("Gui hoa don thanh cong !");
+        }
+    }
+}
---
+++ Facade/Facade/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FacadeDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+
+            Console.Write("\n--------------------------\n");
+            Console.WriteLine("Chuong trinh nay dang chay");
+            Console.Read();
+        }
+    }
+}
